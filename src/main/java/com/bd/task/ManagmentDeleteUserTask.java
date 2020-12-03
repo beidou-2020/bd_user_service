4ced5f63191104ce04401fb876baa496bb930711
beidou <18513032646@163.com>
2020-12-03 11:43:07 +0800

    调用存储过程：手动和定时任务销毁已被删除用户的密码信息

--- src/main/java/com/bd/task/ManagmentDeleteUserTask.java
+++ src/main/java/com/bd/task/ManagmentDeleteUserTask.java
@@ -1,5 +1,6 @@
 package com.bd.task;
 
+import com.alibaba.fastjson.JSONObject;
 import com.bd.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
@@ -22,11 +23,12 @@ public class ManagmentDeleteUserTask {
      * 每月1、15号凌晨4点整销毁一次，已被删除用户的密码信息。
      */
     @Async("destoryDelUserPwdTaskExecutor")
-    // @Scheduled(cron = "0 0 4 1,15 * * *")           // 每月1、15号凌晨4点执行一次
-    // @Scheduled(cron = "0 5 * * * ? ")
+    @Scheduled(cron = "0 0 4 1,15 * * ")           // 每月1、15号凌晨4点执行一次
+    // @Scheduled(cron = "0 0/5 * * * ? ")
     public void destoryInvalidUserPwd(){
-        log.info("销毁被删除用户的密码信息===begin");
-        userService.destoryInvalidUserPwdByProce();
-        log.info("销毁被删除用户的密码信息===end");
+        log.info("beidou.user_service_pack.destoryInvalidUserPwd存储过程===begin");
+        Integer dealWithNum = userService.destoryInvalidUserPwdByProce();
+        log.info("本次销毁已被删除用户的密码的记录数：{}", JSONObject.toJSONString(dealWithNum));
+        log.info("beidou.user_service_pack.destoryInvalidUserPwd存储过程===end");
     }
 }
