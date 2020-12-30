5addb940945567ec20399449174d29bc7d538afb
beidou <18513032646@163.com>
2020-12-30 10:49:10 +0800

    部署：本地生产环境

--- src/main/java/com/bd/BdUserServiceApplication.java
+++ src/main/java/com/bd/BdUserServiceApplication.java
@@ -3,13 +3,12 @@ package com.bd;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = {"com.bd.*"})
 @MapperScan("com.bd.repository")
 @EnableTransactionManagement    //开启事务注解
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
 public class BdUserServiceApplication {
 
     public static void main(String[] args) {
